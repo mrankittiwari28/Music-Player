4ab5ad39cd8a681a6c4672d9cd70e8b981b65dc6
Ankit Tiwari <123309987+mrankittiwari28@users.noreply.github.com>
2023-06-02 22:39:39 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,33 @@
+        let songs = [
+    {
+        name: 'song1',
+        path: 'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview123/v4/0a/82/fe/0a82feaf-ab78-c268-d91d-65f698b91ce1/mzaf_3492393954382629146.plus.aac.p.m4a',
+        artist: 'artist 1',
+        cover: 'https://th.bing.com/th/id/R.996681fbc703d2ae9f757e11ac4f9258?rik=x7RteJ7Urp9%2fKQ&riu=http%3a%2f%2fwww.wallpapergeeks.com%2fwp-content%2fuploads%2f2014%2f05%2fcool-3D-wallpaper.jpg&ehk=VAJ4ZY%2fbRzVf3y3WVZnvtnX%2fpMAcozmuI1YWr%2bP6tU8%3d&risl=&pid=ImgRaw&r=0'
+    },
+    {
+        name: 'song2',
+        path: 'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview123/v4/22/e5/8e/22e58eeb-5916-c584-257c-735e1b4b0175/mzaf_17966999117302129087.plus.aac.p.m4a',
+        artist: 'artist2',
+        cover: 'https://archziner.com/wp-content/uploads/2020/07/air-jordan-hoodie-worn-by-man-wearing-purge-mask-with-neon-lights-super-cool-wallpapers-holding-pink-smoke-bomb.jpg'
+    },
+    {
+        name: 'song3',
+        path: 'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview123/v4/72/cf/5a/72cf5ad7-e1b3-d6ce-84b1-973d0b52d10b/mzaf_2144917941139718442.plus.aac.p.m4a',
+        artist: 'artist2',
+        cover: 'https://wallpapers.com/images/featured/fz4qiypiy3ob4vix.jpg'
+    },
+    {
+        name: 'song4',
+        path: 'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview123/v4/d0/0b/d2/d00bd29b-1a4a-239b-23df-b3df6bb7d30e/mzaf_16326686631763178212.plus.aac.p.m4a',
+        artist: 'artist2',
+        cover: 'https://gratisography.com/wp-content/uploads/2023/01/gratisography-cool-balloon-free-stock-photo.jpg'
+    },
+    {
+        name: 'song5',
+        path: 'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview123/v4/c8/cc/64/c8cc640a-6ae6-9f28-def2-05588c7f43dc/mzaf_520533846207392561.plus.aac.p.m4a',
+        artist: 'artist2',
+        cover: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRxCpsxN9WdwRzagwiDsD3Ie_7heTbaVXn-WxK8H2v&s'
+    }
+    
+]
